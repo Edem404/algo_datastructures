f1d98dff9dd24b7c2bbd7ebaf6557259514fcbcd
love_with <rbordulyak@gmail.com>
2023-11-03 09:50:18 +0200

    now function fits in one screen if not taken dockstring into account

--- fourth_lab/src/min_moves_count.py
+++ fourth_lab/src/min_moves_count.py
@@ -5,33 +5,43 @@ import collections
 COUNT_OF_POSSIBLE_MOVES = 8
 
 
-def min_moves_count(param_file, path_to_result):
+def read_file(param_file):
     """
-    function what solve problem about chess knight minimum moves using bfs
-
-    time complexity - count of vertexes * count of edges.
-    Edge = cell of chess board
-    Vertex = possible move of knight = 8
-    So time complexity -> V * E or 8 * E or E(if it is possible to use=)
-
-    :param param_file: path to file with arguments
-    :param path_to_result: path to file with result
-    :return: moves: INT minimal count of moves need to reach target cell on chess board
+        reading file and getting param
+    :param param_file: path to file
+    :return: tuple of params need for solve problem in func min_moves_count()
     """
     with open(param_file, "r", encoding="utf-8") as file:
         lines = file.readlines()
         board_size = int(lines[0].strip())
         start_position = tuple(map(int, lines[1].strip().split(",")))
-        target_position = tuple(map(int, lines[2].strip().split(",")))
+        end_position = tuple(map(int, lines[2].strip().split(",")))
 
-        possible_moves_x = [2, 2, -2, -2, 1, 1, -1, -1]
-        possible_moves_y = [-1, 1, 1, -1, 2, -2, 2, -2]
+    return board_size, start_position, end_position
 
-        visited = set()
-        queue = collections.deque()
 
-        visited.add(start_position)
-        queue.append((start_position, 0))
+def min_moves_count(param_file, path_to_result):
+    """
+        function what solve problem about chess knight minimum moves using bfs
+
+        time complexity - count of vertexes * count of edges.
+        Edge = possible moves of knight (8)
+        Vertex = cell of chess board
+        So time complexity -> V * E or 8 * E or E(if it is possible to use=)
+    :param param_file: path to file with arguments
+    :param path_to_result: path to file with result
+    :return: moves: INT minimal count of moves need to reach target cell on chess board
+    """
+    board_size, start_position, target_position = read_file(param_file)
+
+    possible_moves_x = [2, 2, -2, -2, 1, 1, -1, -1]
+    possible_moves_y = [-1, 1, 1, -1, 2, -2, 2, -2]
+
+    visited = set()
+    queue = collections.deque()
+
+    visited.add(start_position)
+    queue.append((start_position, 0))
     while queue:
         current_position, moves = queue.popleft()
 
